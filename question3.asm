1dcd13a69f802a0c5fdd68dca828c88b0942b0b5
georgeo30 <52928139+georgeo30@users.noreply.github.com>
2019-10-01 18:49:26 +0200

    A5 C3 : q3 Working

--- question3.asm
+++ question3.asm
@@ -2,26 +2,77 @@
 #question3.asm --  program to find weather two numbers are relatively prime.
 #registers used:$a0--syscall param
 #		$v0--syscall param and return
-#		$t0--first int
-#		$t1--second int
+#		$t0--input
+#		$t1--loop counter to test both words
+#		$t2--mid point of number
+#		$t3--internal incrementer
+#		$t4--tempvar
+#		$t5-- int 2
+#		$t6--second varr temp
 
 	.text
 main:
+	li	$t5, 2
 	la 	$a0, first_msg		#first_msg to a0
 	li	$v0, 4			#print_string
 	syscall
 
 	li	$v0, 5			#read_int
-	move	$t0, $v0		#moving to t0,first int
 	syscall
+	move	$t0, $v0		#moving to t0,first int
+	
+	add	$t1, $t1, 1		#increment t1
+	
 
 	la 	$a0, second_msg		#second_msg to a0
 	li	$v0, 4			#print_string
 	syscall
 
 	li	$v0, 5			#read_int
-	move	$t1, $v0		#moving to t0
 	syscall
+	move	$t7, $v0		#moving to t0
+
+	b checkprime
+
+return: 				#get second input
+	
+
+	move	$t0, $t7		#moving to t0
+	
+	add	$t1, $t1, 1		#increment t1
+	
+	b checkprime
+	
+checkprime:
+	li	$t3, 2
+	div	$t0, $t5		#dividing the int
+	mflo	$t2			#storing in t2
+	b internalcheck			#branches to actual check if both words havent been checked
+
+internalcheck:
+	blt	$t3, $t2, primeornot		#checking if all primes have been cheked
+	bne	$t1, 2, return		#checking of both words have been checked
+	b printyes
+
+primeornot:	
+	div 	$t0, $t3
+	mfhi	$t4
+	beq	$t4, 0, printnot
+	add	$t3, $t3, 1
+	b internalcheck
+
+printyes:
+	la 	$a0, prime_msg		#prime_msg to a0
+	li	$v0, 4			#print_string
+	syscall
+	b exit
+
+printnot:
+	la 	$a0, nonprime_msg	#nonprime_msg to a0
+	li	$v0, 4			#print_string
+	syscall
+	b exit
+
 exit:
 	li	$v0, 10
 	syscall
@@ -29,6 +80,6 @@ exit:
 	.data
 first_msg:	.asciiz "Enter the first number: \n"
 second_msg:	.asciiz "Enter the second number: \n"
-prime_msg:	.asciiz "The entered numbers are relatively prime."
-nonprime_msg:	.asciiz "The entered numbers are not relatively prime."
+prime_msg:	.asciiz "The two numbers are relatively prime"
+nonprime_msg:	.asciiz "The two numbers are not relatively prime"
 
