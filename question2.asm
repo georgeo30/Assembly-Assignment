16a63601cc59d2aece0cb6c9e3f96abb60712db4
georgeo30 <52928139+georgeo30@users.noreply.github.com>
2019-10-01 11:52:34 +0200

    A5 C8 : Working

--- question2.asm
+++ question2.asm
@@ -25,13 +25,46 @@ loop:					#loop for 5 input
 	
 	div	$t4, $t0		#divide input by 2
 	mfhi	$t5			#get remainder
-	div	$t4, $t3		#divide input by 3
+	div	$t4, $t1		#divide input by 3
 	mfhi	$t6			#get remainder
+	
+	bne	$t5, $t6, eitherOne	#checking if t5 and t6 is not equal jump to individual
+	
+	la	$a0, both_msg		#giving the address of both_msg
+	li	$v0, 4			#assigning 4 to v0
+	syscall 
 
 	add	$t3, $t3, 1		#incrementing input counter
 	b loop
-	
 
+
+eitherOne: 				#it can only be 3 or 2
+	beq	$t5, 0, itsTwo		#if mod against 2
+	beq	$t6, 0, itsThree	#if mode against 3
+	la	$a0, neither_msg	#giving the address of neither_msg
+	li	$v0, 4			#assigning 4 to v0
+	syscall
+	add	$t3, $t3, 1		#incrementing input counter
+	b loop 
+	 
+
+itsThree:				#its Three
+
+	la	$a0, three_msg		#giving the address of three_msg
+	li	$v0, 4			#assigning 4 to v0
+	syscall 
+	add	$t3, $t3, 1		#incrementing input counter
+	b loop
+	
+itsTwo:					#its Two
+	
+	la	$a0, two_msg		#giving the address of two_msg
+	li	$v0, 4			#assigning 4 to v0
+	syscall 
+	add	$t3, $t3, 1		#incrementing input counter
+	b loop
+
+	
 exit:
 	li	$v0, 10
 	syscall
@@ -40,4 +73,9 @@ exit:
 
 enter_msg:	.asciiz "Enter a number: "
 output.msg:	.asciiz "It is divisible by "
+both_msg:	.asciiz	"It is divisible by both 2 and 3 \n"
+two_msg:	.asciiz "It is divisible by 2 \n"
+three_msg:	.asciiz "It is divisible by 3 \n"
+neither_msg:	.asciiz "It is neither divisible by 2 nor 3 \n"
+
 new_line:	.asciiz "\n"
