4a289e5f3562c89ef130491f567ea17c23c1aeba
georgeo30 <52928139+georgeo30@users.noreply.github.com>
2019-10-01 11:23:26 +0200

    A5 C7  5 inputs and remainder working

--- question2.asm
+++ question2.asm
@@ -9,19 +9,27 @@
 main:					#main method
 	li	$t0, 2			#storing 2 to t0
 	li	$t1, 3			#storing 3 to t1
-	li	$t2, 6			#storing 6 to t2 to get 5 inputs
+	li	$t2, 5			#storing 6 to t2 to get 5 inputs
+	li 	$t3, 0
 
 loop:					#loop for 5 input
 	
-	
+	beq	$t3, $t2, exit
 	la	$a0, enter_msg		#giving the address of enter_msg
 	li	$v0, 4			#assigning 4 to v0
 	syscall 			#syscall
 
 	li	$v0, 5			#get_int
 	syscall
-	move	$t2, $v0		#moving the input to register $t1
+	move	$t4, $v0		#moving the input to register $t1
 	
+	div	$t4, $t0		#divide input by 2
+	mfhi	$t5			#get remainder
+	div	$t4, $t3		#divide input by 3
+	mfhi	$t6			#get remainder
+
+	add	$t3, $t3, 1		#incrementing input counter
+	b loop
 	
 
 exit:
